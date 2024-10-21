b3c68d7177e5c0b6f986e555eac351b68995fd1b
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-10-21 16:03:47 +0530

    mongoose error solve

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
-
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/CRMdb', {
-    useNewUrlParser: true
+    useUnifiedTopology: true // Keep this option for the new connection management engine
 });
 
 // bodyparser setup
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app); 
